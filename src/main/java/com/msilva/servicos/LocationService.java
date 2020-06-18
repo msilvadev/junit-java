91ea62ab132d10c9f42a9e1784b924d1bef6e795
Matheus Silva <msilva.jus@gmail.com>
2020-06-18 18:50:37 -0300

    refactor

--- src/main/java/com/msilva/servicos/LocationService.java
+++ src/main/java/com/msilva/servicos/LocationService.java
@@ -6,54 +6,75 @@ import com.msilva.entities.User;
 import com.msilva.exceptions.LocadoraException;
 import com.msilva.exceptions.MovieWithoutStockException;
 
-import java.util.Date;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
 
 import static com.msilva.utils.DataUtils.adicionarDias;
 
 public class LocationService {
-	
-	public Location locationMovie(User user, List<Movie> movies) throws MovieWithoutStockException, LocadoraException {
-		if(user == null) {
-			throw new LocadoraException("Empty user");
-		}
-		
-		if(movies == null || movies.isEmpty()) {
-			throw new LocadoraException("Movie empty");
-		}
-		
-		for(Movie movie : movies) {
-			if(movie.getEstoque() == 0) {
-				throw new MovieWithoutStockException();
-			}
-		}
-		
-		Location location = new Location();
-		location.setFilmes(movies);
-		location.setUsuario(user);
-		location.setDataLocacao(new Date());
-		Double valorTotal = 0d;
-		for(int i = 0; i < movies.size(); i++) {
-			Movie movie = movies.get(i);
-			Double valorFilme = movie.getPrecoLocacao();
-			switch (i) {
-				case 2: valorFilme = valorFilme * 0.75; break;
-				case 3: valorFilme = valorFilme * 0.5; break;
-				case 4: valorFilme = valorFilme * 0.25; break;
-				case 5: valorFilme = 0d; break;
-			}
-			valorTotal += valorFilme;
-		}
-		location.setValor(valorTotal);
-		
-		//Entrega no dia seguinte
-		Date deliveryDate = new Date();
-		deliveryDate = adicionarDias(deliveryDate, 1);
-		location.setDataRetorno(deliveryDate);
-		
-		//Save location...
-		//TODO add method to save
-		
-		return location;
-	}
+
+    protected static Map<Integer, Function<Double, Double>> calculatePercentage = new HashMap<>();
+
+    public LocationService() {
+        calculatePercentage.put(2, (x) -> calculate2(x));
+        calculatePercentage.put(3, (x) -> calculate2(x));
+        calculatePercentage.put(4, (x) -> calculate2(x));
+        calculatePercentage.put(5, (x) -> calculate2(x));
+    }
+
+    public Location locationMovie(User user, List<Movie> movies) throws MovieWithoutStockException, LocadoraException {
+        verifyInitial(user, movies);
+
+        Location location = new Location();
+        location.setFilmes(movies);
+        location.setUsuario(user);
+        location.setDataLocacao(new Date());
+        Double valorTotal = 0d;
+        for (int i = 0; i < movies.size(); i++) {
+            if (movies.get(i).getEstoque() == 0) {
+                throw new MovieWithoutStockException();
+            }
+            Movie movie = movies.get(i);
+            Double movieValue = movie.getPrecoLocacao();
+            movieValue = calculatePercentage.getOrDefault(i, null).apply(movieValue);
+            valorTotal += movieValue;
+        }
+        location.setValor(valorTotal);
+
+        Date deliveryDate = new Date();
+        deliveryDate = adicionarDias(deliveryDate, 1);
+        location.setDataRetorno(deliveryDate);
+
+        //Save location...
+        //TODO add method to save
+
+        return location;
+    }
+
+    private void verifyInitial(User user, List<Movie> movies) throws LocadoraException {
+        if (user == null) {
+            throw new LocadoraException("Empty user");
+        }
+
+        if (movies == null || movies.isEmpty()) {
+            throw new LocadoraException("Movie empty");
+        }
+    }
+
+    private Double calculate2(double movieValue) {
+        return movieValue * 0.75;
+    }
+
+    private Double calculate3(double movieValue) {
+        return movieValue * 0.5;
+    }
+
+    private Double calculate4(double movieValue) {
+        return movieValue * 0.25;
+    }
+
+    private Double calculate5(double movieValue) {
+        return 0d;
+    }
+
 }
